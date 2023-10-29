a477f667de0e17bfa539998d51b209d527e78047
AmritDuwal1 <61304552+AmritDuwal1@users.noreply.github.com>
2023-10-29 07:22:21 +0545

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# PocketLabImageProcessing
+
+A description of this package.
---
+++ Sources/PocketLabImageProcessing/PocketLabImageProcessing.swift
@@ -0,0 +1,6 @@
+public struct PocketLabImageProcessing {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+    }
+}
